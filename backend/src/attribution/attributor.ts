3f4195d0c1946133f62096e7dc83936581eeedab
dennis <d.g.sprokholt@student.rug.nl>
2017-03-08 19:08:21 +0100

    Backend: Linked with GLAD

--- backend/src/attribution/attributor.ts
+++ backend/src/attribution/attributor.ts
@@ -6,11 +6,39 @@
  * IMPORTANT: Attributor is not currently linked with the GLAD system. Only
  * dummy output values are returned.
  */
+
+import * as child_process from 'child_process'; //require('child_process').execFile;
+const execFile = child_process.execFile;
+
  
 import { FromClient, ToClient } from './network_interface';
 
 export class Attributor {
   constructor( ) { }
+  
+  // Callback that gets called once the Python program has finished
+  // TODO: Wrap this with a task class
+  private programFinishedCallback( callback, error, stdout, stderr ) {
+    if ( error ) {
+      callback( 'An error occurred' );
+      return;
+    }
+    
+    // Hackerish extraction of Python output
+    // Clean this up after input/output rules are better established
+    try {
+      var arr = /.*((0|1)\.(\d*)).*((0|1)\.(\d*)).*/g.exec( stdout );
+      let probability = parseFloat( arr[4] );
+      
+      let output: ToClient = {
+        sameAuthor: true,
+        confidence: probability
+      };
+      callback( output );
+    } catch ( ex ) {
+      callback( 'An error occurred' );
+    }
+  }
   
   public handleRequest( request: FromClient, callback: ( out: any ) => void ) {
     if ( !this.isValid_FromClient( request ) ) {
@@ -18,13 +46,13 @@ export class Attributor {
       return;
     }
     
-    // Only dummy output is currently used.
-    // TODO: Link Attributor with the GLAD system
-    let output: ToClient = {
-      sameAuthor: true,
-      confidence: 0.56
-    };
-    callback( output );
+    const args = [ 'glad-copy.py',
+                   '--inputknown', request.knownAuthorText,
+                   '--inputunknown', request.unknownAuthorText,
+                   '-m', 'models/default' ];
+    const options = { cwd: 'resources/glad' };
+    
+    execFile( 'python3', args, options, this.programFinishedCallback.bind( this, callback ) );
   }
   
   /**
