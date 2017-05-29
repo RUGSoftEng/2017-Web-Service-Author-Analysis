ec4b831b4fda9ebab2099bd2f3dd4251489b1d7f
Folkert de Vries <folkert@folkertdev.nl>
2017-05-29 17:04:32 +0200

    small refactoring display nice names for languages and feature sets put more stuff in I18n use the same footer on all pages

--- frontend/src/Pages/Home.elm
+++ frontend/src/Pages/Home.elm
@@ -8,7 +8,7 @@ import Bootstrap.Grid.Col as Col
 import Bootstrap.Navbar as Navbar
 import I18n exposing (Translation)
 import Route
-import Views.Page exposing (mainNav)
+import Views.Page exposing (mainNav, viewFooter)
 
 
 textCenter =
@@ -16,53 +16,42 @@ textCenter =
 
 
 view : Translation -> Html ()
-view _ =
-    div [ class "home maincontainer" ]
-        [ header [ class "header", id "home", attribute "style" "min-height: 76px;" ]
-            [ mainNav (\_ -> ()) (Tuple.first <| Navbar.initialState (\_ -> ()))
-            , div [ class " home-header-wrap" ]
-                [ div [ class "header-content-wrap" ]
-                    [ div [ class "container" ]
-                        [ h1 [ class "intro-text" ]
-                            [ text "Author Analysis" ]
+view translation =
+    let
+        t key =
+            I18n.get translation key
+
+        -- empty navbar state, we don't use it anyway
+        navbarState =
+            Tuple.first <| Navbar.initialState (\_ -> ())
+    in
+        div [ class "home maincontainer" ]
+            [ header [ class "header", id "home", attribute "style" "min-height: 76px;" ]
+                [ mainNav (\_ -> ()) navbarState
+                , div [ class " home-header-wrap" ]
+                    [ div [ class "header-content-wrap" ]
+                        [ div [ class "container" ]
+                            [ h1 [ class "intro-text" ]
+                                [ text "Author Analysis" ]
+                            ]
+                        ]
+                    , div [ class "clear" ]
+                        []
+                    ]
+                ]
+            , Grid.container [ style [ ( "width", "60%" ) ] ]
+                [ Grid.row [ Row.topXs ]
+                    [ Grid.col [ textCenter, Col.attrs [ id "attribution-square" ] ]
+                        [ span [] [ text "Attribution" ]
+                        , text (t "attribution")
+                        ]
+                    , Grid.col [ textCenter, Col.attrs [ id "profiling-square" ] ]
+                        [ span [] [ text "Profiling" ]
+                        , text (t "profiling")
                         ]
                     ]
-                , div [ class "clear" ]
-                    []
+                , Grid.row [] [ Grid.col [] [ text (t "rationale") ] ]
                 ]
+            , viewFooter navbarState
+                |> Html.map (\_ -> ())
             ]
-        , Grid.container [ style [ ( "width", "60%" ) ] ]
-            [ Grid.row [ Row.topXs ]
-                [ Grid.col [ textCenter, Col.attrs [ id "attribution-square" ] ]
-                    [ span [] [ text "Attribution" ]
-                    , text """ given one or more texts that we know are written by the same person, the system
-                    will predict whether a new, unknown text is also written by the same person.
-                    """
-                    ]
-                , Grid.col [ textCenter, Col.attrs [ id "profiling-square" ] ]
-                    [ span [] [ text "Profiling" ]
-                    , text """ given a text the system will predict the gender and age of the author."""
-                    ]
-                ]
-            , Grid.row []
-                [ Grid.col []
-                    [ text """
-Author analysis is relevant in literature studies, modern
-and old, in law, when working with social media
-contexts, politics, and any other field where
-identifying who wrote something provides valuable
-information. It also relates to the currently very hot
-topic of alternative news.
-"""
-                    ]
-                ]
-            ]
-        , footer [ id "footer" ]
-            [ div [ class "container" ]
-                [ div [ class "col-md-3 company-details" ]
-                    [ div [ class "zerif-footer-address" ]
-                        [ text "" ]
-                    ]
-                ]
-            ]
-        ]
